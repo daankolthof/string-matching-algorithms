ada8d96083e5b0698fed8edc414851a8f09caadd
Daan Kolthof <daan_kolthof@hotmail.com>
2019-08-12 23:42:00 +0100

    Naive implementation of string matching.

---
+++ string-matching-algorithms/NaiveMatching.h
@@ -0,0 +1,42 @@
+#ifndef NAIVE_MATCHING
+#define NAIVE_MATCHING
+
+#include <string>
+
+#include <iostream>
+
+class NaiveMatching
+{
+public:
+
+	/* Finds the given pattern in a string, returning starting index of the pattern in the given string.
+	 * Will return -1 if the pattenr was not found in the string.
+	 * Uses naive O(n*m) algorithm, with n: size of string, m: size of pattern.
+	*/
+	static int findString(const std::string& string, const std::string& pattern)
+	{
+		for (size_t i = 0; i < string.length() - pattern.length() + 1; i++)
+		{
+			// For this starting position in the string, iterate over the entire pattern, looking for a match.
+			bool match = true;
+			for (size_t j = 0; j < pattern.length() && match; j++)
+			{
+				if (string[i+j] != pattern[j])
+				{
+					match = false;
+				}		
+			}
+
+			if (match)
+			{
+				// A match was found, return the string index at which the pattern starts.
+				return i;
+			}
+		}
+
+		// No match was found, return -1 to indicate this.
+		return -1;
+	}
+};
+
+#endif //NAIVE_MATCHING
