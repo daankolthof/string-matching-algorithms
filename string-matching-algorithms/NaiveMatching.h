9875d80ea6c43e177a5a11a7b02ade9b9c852bf4
Daan Kolthof <daan_kolthof@hotmail.com>
2019-08-17 16:46:34 +0100

    Small cleanup of naive matching code.

--- string-matching-algorithms/NaiveMatching.h
+++ string-matching-algorithms/NaiveMatching.h
@@ -13,11 +13,11 @@ public:
 	*/
 	static int findString(const std::string& string, const std::string& pattern)
 	{
-		for (size_t i = 0; i < string.length() - pattern.length() + 1; i++)
+		for (size_t i = 0; i <= string.length() - pattern.length(); i++)
 		{
 			// For this starting position in the string, iterate over the entire pattern, looking for a match.
 			bool match = true;
-			for (size_t j = 0; j < pattern.length() && match; j++)
+			for (size_t j = 0; match && j < pattern.length(); j++)
 			{
 				if (string[i+j] != pattern[j])
 				{
