d2bef88b9d4b62f4072fcc5c4296c7719fcb55bf
Daan Kolthof <daan_kolthof@hotmail.com>
2019-08-17 14:07:57 +0100

    Changed algorithm descriptions within code.

--- string-matching-algorithms/KnuthMorrisPratt.h
+++ string-matching-algorithms/KnuthMorrisPratt.h
@@ -9,7 +9,8 @@ class KnuthMorrisPratt
 public:
 
 	/* Finds the given pattern in a string, returning starting index of the pattern in the given string.
-	 * Will return -1 if the pattenr was not found in the string.
+	 * Will return -1 if the pattern was not found in the string.
+	 * Uses Knuth-Morris-Pratt algorithm, with a runtime of O(n+m), with n: size of string.
 	*/
 	static int findString(const std::string& string, const std::string& pattern)
 	{
@@ -85,7 +86,5 @@ private:
 
 
 };
-
-
 
 #endif //KNUTH_MORRIS_PRATT_H
--- string-matching-algorithms/NaiveMatching.h
+++ string-matching-algorithms/NaiveMatching.h
@@ -10,7 +10,7 @@ class NaiveMatching
 public:
 
 	/* Finds the given pattern in a string, returning starting index of the pattern in the given string.
-	 * Will return -1 if the pattenr was not found in the string.
+	 * Will return -1 if the pattern was not found in the string.
 	 * Uses naive O(n*m) algorithm, with n: size of string, m: size of pattern.
 	*/
 	static int findString(const std::string& string, const std::string& pattern)
